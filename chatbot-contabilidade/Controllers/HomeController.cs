0e9726d720276fe8c9391c2549e6ae095306e103
rckhz <alejandrolima123@gmail.com>
2026-05-21 00:45:36 -0300

    commit 1

---
+++ chatbot-contabilidade/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using chatbot_contabilidade.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace chatbot_contabilidade.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
